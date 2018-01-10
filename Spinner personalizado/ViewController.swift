32b2ebbe5b1fbc569b5ab91aba71649a1800740a
Fran <asensi.benito@gmail.com>
2018-01-10 11:00:46 +0100

    Initial Commit

---
+++ Spinner personalizado/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Spinner personalizado
+//
+//  Created by Francisco Javier Asensi Benito on 10/1/18.
+//  Copyright © 2018 Francisco Javier Asensi Benito. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
